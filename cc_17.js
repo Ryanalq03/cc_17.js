a02aaf2cf9f1edad97b7889c0fc02d3540936a1e
Ryan Alqasemi <alqasemir@usf.edu>
2025-03-27 20:17:09 -0400

    Task 2 Complete and html update

--- cc_17.js
+++ cc_17.js
@@ -1,5 +1,11 @@
 //Coding Challenge #17
 
+//Helper function to display details
+function logMessage(message) {
+    const outputDiv = document.getElementById('output');
+    // Append the message with a line break
+    outputDiv.innerText += message + "\n";
+  }
 //Task 1 Customer Class
 class Customer { //Creates Customer class
     constructor(name, email) {
@@ -23,3 +29,23 @@ customer1.addPurchase(100);
 customer1.addPurchase(200);
 console.log("Customer Created:", customer1);
 console.log("Alice Total Spent: $", customer1.getTotalSpent());
+
+//Task 2 SalesRep Class
+
+class SalesRep { //Creates class
+    constructor(name) {
+      this.name = name;
+      this.clients = []; // Array of Customer objects
+    }
+    
+    // Adds a customer to list of clients
+    addClient(customer) {
+      this.clients.push(customer);
+    }
+    
+    // Finds the client by name and returns their total spent
+    getClientTotal(name) {
+      const client = this.clients.find(cust => cust.name === name);
+      return client ? client.getTotalSpent() : 0;
+    }
+  }
